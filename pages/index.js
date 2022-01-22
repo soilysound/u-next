377992055ba65da821a8485a15b25560ceb3beb4
mark groak <markgroak@marks-MacBook.local>
2022-01-22 11:00:56 +0000

    update title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Unredacted</title>
         <link rel="icon" href="/favicon.ico" />
 				<link rel="stylesheet" href="/css/styles.css" />
       </Head>
