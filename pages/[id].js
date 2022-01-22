ba4872db97014b633f72426fdd368158bd1c7e79
mark groak <markgroak@marks-MacBook.local>
2022-01-22 13:19:06 +0000

    add stories

--- pages/[id].js
+++ pages/[id].js
@@ -1,22 +1,35 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-export default function Article() {
+const stories = require('../stories.json');
+
+export default function Article(props) {
 
 	const router = useRouter()
 	const { id } = router.query;
+	let story = {};
+	story = stories.stories.filter((s) => {
+		return s.urlroot === id;
+	});
 
   return (
     <>
       <Head>
-        <title>{id}</title>
+        <title>{story[0].title}</title>
       </Head>
-
-      
-        <h1 className="title">
-          {id}
-        </h1>
-			
-	</>
+			<Link href="/"><a href="">back</a></Link>
+			<h1 className="title">
+				<div>{story[0].subtitle}</div>
+				{story[0].title}
+			</h1>
+		</>
   )
 }
+
+Article.getInitialProps = async function() {
+  
+  return {
+    shows: {}
+  };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from 'next/head';
+import Link from 'next/link';
+
+const stories = require('../stories.json');
 
 export default function Home() {
+
+	const storylist = stories.stories.map(story => (
+    <div><Link href={story.urlroot}><a>{story.title} - {story.subtitle}</a></Link></div>
+  ));
+
   return (
-    <>
-      <Head>
-        
-      </Head>
-
-     
-				<Link href="/bum/"><a>Bum></a></Link>
-     
-
+    <>	
+			{storylist}
     </>
   )
 }
