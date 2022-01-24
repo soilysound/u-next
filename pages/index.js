3a8c6674821a3c8b376de11012bf38496657be24
mark groak <markgroak@marks-MacBook.local>
2022-01-24 20:47:42 +0000

    fix dark mode

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({ stories }) {
 			<Head>
 				<title>Unredacted</title>
 			</Head>
-			<div className="flex-grid" style={{ "--basis": "250px" }}>
+			<div className="flex-grid" style={{ "--basis": "250px", "--gap": "var(--gap-300)" }}>
 				{storylist}
 			</div>
 		</>
