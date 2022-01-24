2bcc2fdc4120fad1c59272ce19c4f99641ca2968
mark groak <markgroak@marks-MacBook.local>
2022-01-24 13:42:16 +0000

    add index styles

--- pages/components/tile.js
+++ pages/components/tile.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Tile({story}) {
   return (
-		<article className="tile" key={story.title}>
+		<article className="tile">
 				<figure className="flex-grid"  style={{"--gap": "0px"}}>
 					<img width="400" height="400" aria-hidden="true" loading="lazy" src={`/img/stories/${story.path}.webp`} />
 					<figcaption className="flex-grid" style={{"--gap": "0px"}}>
