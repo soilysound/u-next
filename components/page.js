e0aa486aaaa6e3c42b684dea23beb92c7867cc1d
AidanZealley <aidan@zealley.com>
2022-01-25 14:12:16 +0000

    add missing changes

--- components/page.js
+++ components/page.js
@@ -1,7 +1,26 @@
+import { useNavContext } from 'components/nav/nav-provider';
+import { useEffect } from 'react';
+import { useSpring, animated  } from 'react-spring';
+
 export default function Page({ children }) {
+  const { navRef, navOpenState } = useNavContext()
+  const [pageShiftStyles, api] = useSpring(() => ({
+    transform: 'translateY(0)',
+  }));
+
+  useEffect(() => {
+    if (!navRef.current) {
+			return
+		}
+
+    api.start({
+      transform: `translateY(${!navOpenState ? navRef.current.offsetHeight : '0'}px)`
+    })
+  }, [api, navRef, navOpenState]);
+
   return (
-    <div>
+    <animated.div style={pageShiftStyles}>
       {children}
-    </div>
+    </animated.div>
   );
 }
