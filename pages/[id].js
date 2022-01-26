ae38ab19d79778ae1dbbf5e74ddcf71fcfa68f6a
mark groak <markgroak@marks-MacBook.local>
2022-01-26 23:26:24 +0000

    add missing story images

--- pages/[id].js
+++ pages/[id].js
@@ -34,13 +34,12 @@ export default function Article(props) {
 						<div className="article-byline-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 43 43" fill="white"><path d="M39.561 33.971l-.145.174c-4.774 5.728-11.133 8.884-17.902 8.884-6.77 0-13.128-3.155-17.903-8.884l-.144-.174.034-.223c.922-6.014 4.064-10.845 8.847-13.606l.34-.196.271.284c2.259 2.37 5.297 3.674 8.554 3.674s6.295-1.305 8.554-3.674l.271-.284.34.196c4.783 2.761 7.925 7.592 8.848 13.606l.035.223zM21.514 21.489c5.924 0 10.744-4.82 10.744-10.744C32.258 4.821 27.438 0 21.514 0S10.77 4.821 10.77 10.744s4.82 10.745 10.744 10.745z"></path></svg></div><strong>theunredacted</strong> {story.formatteddate}</div>
 
 				</div>
-			<div className="wrap article-body article-text" style={{ "--wrap-gap": "var(--gap-400)" }} dangerouslySetInnerHTML={{ __html: story.body }} />
+			<div className="wrap article-body article-text" style={{ "--wrap-gap": "var(--gap-400)" }} dangerouslySetInnerHTML={{ __html: `${story.body}<p class="article-end supporting-text"><span>${story.snippet}</span> - <a href="#comments">add your comment below</a></p>` }} />
 			<div className="article-related component-padding page-canvas-shade wrap wrap-full-width" style={{"--component-padding-value": "var(--component-padding-loose)"}}>
 				<h2 className="display-400 flex-grid wrap" style={{"--justify": "center"}}>Related stories</h2>
-				
 					<div className="grid wrap article-related-body" style={{ "--basis": "260px", "--wrap-width": "var(--site-width-m)"}}>{storylist}</div>
 			</div>
-			<h2 className="display-400 flex-grid wrap" style={{"--justify": "center"}}>Comments</h2>
+			<h2 className="display-400 flex-grid wrap" style={{"--justify": "center"}} id="comments">Comments</h2>
 			<DiscussionEmbed shortname='unredacted' config={{
 					url: story.baseurl + '/' + story.path,
 					identifier: story.path,
